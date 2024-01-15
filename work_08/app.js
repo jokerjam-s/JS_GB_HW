6756d6051bfc494f20e4b60a542774406d6de18b
Андрей <jokerjam@yandex.ru>
2024-01-15 12:45:35 +0300

    ready

--- work_08/app.js
+++ work_08/app.js
@@ -11,8 +11,13 @@ let basket = new Basket();
 let fitlerPopup = document.querySelector('.filterPopup');
 let fitlerLabel = document.querySelector('.filterLabel');
 let filterIcon = document.querySelector('.filterIcon');
+let basketInfo = document.querySelector("#basketInfo");
+let basketCounter = document.querySelector("#basketCounter");
+let basketButton = document.querySelector("#basket");
+let basketItems = basketInfo.querySelector("#basketItems");
 
-fitlerLabel.addEventListener('click', function() {
+
+fitlerLabel.addEventListener('click', function () {
     fitlerPopup.classList.toggle('hidden');
     fitlerLabel.classList.toggle('filterLabelPink');
     filterIcon.classList.toggle('filterIconPink');
@@ -25,15 +30,15 @@ fitlerLabel.addEventListener('click', function() {
 });
 
 let filterHeaders = document.querySelectorAll('.filterCategoryHeader');
-filterHeaders.forEach(function(header) {
-    header.addEventListener('click', function(event) {
+filterHeaders.forEach(function (header) {
+    header.addEventListener('click', function (event) {
         event.target.nextElementSibling.classList.toggle('hidden');
     })
 });
 
 let filterSizes = document.querySelector('.filterSizes');
 let filterSizeWrap = document.querySelector('.filterSizeWrap');
-filterSizeWrap.addEventListener('click', function() {
+filterSizeWrap.addEventListener('click', function () {
     filterSizes.classList.toggle('hidden');
 });
 
@@ -41,10 +46,38 @@ filterSizeWrap.addEventListener('click', function() {
 // отображение, обработка данных в корзине
 let things = document.querySelectorAll('.featuredItem');
 
-things.forEach(function (thing){
-    thing.addEventListener('click', function(e){
-        if(e.target.tagName === 'BUTTON')
-            alert('dfsfsdf!!! ');
-        //basket.addItem(new BasketItem(''))
+things.forEach(function (thing) {
+    thing.addEventListener('click', function (e) {
+        if (e.target.tagName === 'BUTTON') {
+            const name = thing.querySelector(".featuredName");
+            const price = thing.querySelector(".featuredPrice");
+            basket.addItem(new BasketItem(name.outerText, parseFloat(price.outerText.substring(1))));
+            basketCounter.textContent = basket.counts;
+        }
     })
 });
+
+/*
+ Отображение информации из корзины
+ */
+basketButton.addEventListener("mouseover", function (ev) {
+    basketItems.innerHTML = "";
+    if (basketInfo.style.display === 'none') {
+        basketInfo.style.display = 'block';
+        const total = basketInfo.querySelector("#countThing");
+        const cost = basketInfo.querySelector("#sumCost");
+
+        total.textContent = basket.counts;
+        cost.textContent = basket.cost;
+
+        for (const item of basket.items) {
+            basketItems.innerHTML += `<div>${item.name}</div><div>${item.count}</div><div>${item.price * item.count}</div>`;
+        }
+    }
+});
+
+basketButton.addEventListener("mouseout", function (e){
+    basketInfo.style.display = 'none';
+});
+
+
